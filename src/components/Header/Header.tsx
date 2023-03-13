1dd0f430520f44336518993668204a17fb3694f7
Luisa <luisa.mohle@no.experis.com>
2023-03-13 09:20:10 +0100

    feat: add temporary nav menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,26 @@
+import { NavLink } from "react-router-dom";
+import keycloak from "../../utils/keycloak";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <header>Icon, Searchbar, +</header>
+      {!keycloak.authenticated && <NavLink to="/">Login</NavLink>}
+      {keycloak.authenticated && (
+        <>
+          Temp menu:&nbsp;
+          <NavLink to="/dashboard">Dashboard</NavLink>&nbsp;
+          <NavLink to="/profile">Profile</NavLink>&nbsp;
+          <NavLink to="/profile/settings">Settings</NavLink>&nbsp;
+          <NavLink to="/grouplist">Groups</NavLink>&nbsp;
+          <NavLink to="/group">Group</NavLink>&nbsp;
+          <NavLink to="/topiclist">Topics</NavLink>&nbsp;
+          <NavLink to="/topic">Topic</NavLink>&nbsp;
+          <NavLink to="/thread">Thread</NavLink>&nbsp;
+          <NavLink to="/search">Search</NavLink>
+        </>
+      )}
+    </div>
+  );
 }
 export default Header;
