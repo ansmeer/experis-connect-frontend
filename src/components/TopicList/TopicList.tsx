a2d0491274ad408eb8cde85ad400fc33b7a8d842
Luisa <luisa.mohle@no.experis.com>
2023-03-14 17:17:24 +0100

    feat: show list of topics

--- src/components/TopicList/TopicList.tsx
+++ src/components/TopicList/TopicList.tsx
@@ -1,12 +1,39 @@
-import React from "react";
-import Topic from "../Topic/Topic";
+import { useQuery } from "react-query";
+import { useSelector } from "react-redux";
+import { topicApi } from "../../apis/topicApi";
+import { RootState } from "../../redux/store";
+import { TTopic } from "../../types/topic";
+import Loading from "../Loading/Loading";
+import TopicCard from "../TopicCard/TopicCard";
+import styles from "./topicList.module.css";
 
 function TopicList() {
-  return (
-    <div>
-      <h1>Topic List Page</h1>
-    </div>
-  );
+  const user = useSelector((state: RootState) => state.user.details);
+  const { data, isLoading, isError } = useQuery({
+    queryFn: async (): Promise<TTopic[]> => {
+      const topicRequest = topicApi.get.topics();
+      const response = await fetch(topicRequest.uri, topicRequest.options);
+      return await response.json();
+    },
+  });
+
+  const topicList = data?.map((topic) => (
+    <TopicCard
+      key={topic.id}
+      data={topic}
+      isMember={user?.groups.includes(topic.id) || false}
+    />
+  ));
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isError) {
+    return <div>Could not load topic list.</div>;
+  }
+
+  return <div className={styles.topicList}>{topicList}</div>;
 }
 
 export default TopicList;
