9ab1fb0989a21e1148d3b0d9125f143627c2b6e9
Luisa <luisa.mohle@no.experis.com>
2023-03-15 15:32:51 +0100

    fix: add missing authentication and content-type headers

--- src/apis/topicApi.ts
+++ src/apis/topicApi.ts
@@ -1,50 +1,59 @@
 import { ApiRequestInfo } from "../types/api";
 import { TTopicPost } from "../types/topic";
+import keycloak from "../utils/keycloak";
 
 const topicApiUrl = `${import.meta.env.VITE_API_BASE_URL}/topic`;
 
 export const topicApi = {
   get: {
     topics: (): ApiRequestInfo => {
+      const token = keycloak.token;
       return {
         uri: new URL(topicApiUrl).toString(),
         options: {
           method: "GET",
-          headers: {}, // TODO authentication headers
+          headers: [["Authorization", `Bearer ${token}`]],
         },
       };
     },
     topicById: (topicId: number): ApiRequestInfo => {
+      const token = keycloak.token;
       return {
         uri: new URL(`${topicApiUrl}/${topicId}`).toString(),
         options: {
           method: "GET",
-          headers: {}, // TODO authentication headers
+          headers: [["Authorization", `Bearer ${token}`]],
         },
       };
     },
   },
   post: {
     newTopic: (topicDetails: TTopicPost): ApiRequestInfo => {
+      const token = keycloak.token;
       return {
         uri: new URL(topicApiUrl).toString(),
         options: {
           method: "POST",
-          headers: {}, // TODO authentication headers
+          headers: [
+            ["Authorization", `Bearer ${token}`],
+            ["Content-Type", "application/json"],
+          ],
           body: JSON.stringify(topicDetails),
         },
       };
     },
     addCurrentUserToTopic: (topicId: number): ApiRequestInfo => {
+      const token = keycloak.token;
       return {
         uri: new URL(`${topicApiUrl}/${topicId}/join`).toString(),
         options: {
           method: "POST",
-          headers: {}, // TODO authentication headers
+          headers: [["Authorization", `Bearer ${token}`]],
         },
       };
     },
     addUserToTopic: (topicId: number, userId: string): ApiRequestInfo => {
+      const token = keycloak.token;
       return {
         uri: new URL(
           `${topicApiUrl}/${topicId}/join?` +
@@ -52,7 +61,7 @@ export const topicApi = {
         ).toString(),
         options: {
           method: "POST",
-          headers: {}, // TODO authentication headers
+          headers: [["Authorization", `Bearer ${token}`]],
         },
       };
     },
