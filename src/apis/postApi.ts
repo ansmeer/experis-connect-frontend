a23eab3b8821a6140370c6b652ec2f35f4a27510
Luisa <luisa.mohle@no.experis.com>
2023-03-15 11:34:16 +0100

    fix: include token in post api requests

--- src/apis/postApi.ts
+++ src/apis/postApi.ts
@@ -1,5 +1,6 @@
 import { ApiRequestInfo } from "../types/api";
 import { TPostPost, TPostPut } from "../types/post";
+import keycloak from "../utils/keycloak";
 
 const postApiUrl = `${import.meta.env.VITE_API_BASE_URL}/post`;
 
@@ -7,51 +8,66 @@ const postApiUrl = `${import.meta.env.VITE_API_BASE_URL}/post`;
 export const postApi = {
   get: {
     posts: (): ApiRequestInfo => {
+      const token = keycloak.token;
       return {
         uri: new URL(postApiUrl).toString(),
         options: {
           method: "GET",
-          headers: {}, // TODO authentication headers
+          headers: [["Authorization", `Bearer ${token}`]],
+        },
+      };
+    },
+    postById: (id: number): ApiRequestInfo => {
+      const token = keycloak.token;
+      return {
+        uri: new URL(`${postApiUrl}/${id}`).toString(),
+        options: {
+          method: "GET",
+          headers: [["Authorization", `Bearer ${token}`]],
         },
       };
     },
     searchPosts: (searchTerm: string): ApiRequestInfo => {
+      const token = keycloak.token;
       return {
         uri: new URL(
           `${postApiUrl}?` + new URLSearchParams({ search: searchTerm })
         ).toString(),
         options: {
           method: "GET",
-          headers: {}, // TODO authentication headers
+          headers: [["Authorization", `Bearer ${token}`]],
         },
       };
     },
     postDms: (): ApiRequestInfo => {
+      const token = keycloak.token;
       return {
         uri: new URL(`${postApiUrl}/user`).toString(),
         options: {
           method: "GET",
-          headers: {}, // TODO authentication headers
+          headers: [["Authorization", `Bearer ${token}`]],
         },
       };
     },
     searchPostDms: (searchTerm: string): ApiRequestInfo => {
+      const token = keycloak.token;
       return {
         uri: new URL(
           `${postApiUrl}/user?` + new URLSearchParams({ search: searchTerm })
         ).toString(),
         options: {
           method: "GET",
-          headers: {}, // TODO authentication headers
+          headers: [["Authorization", `Bearer ${token}`]],
         },
       };
     },
     postDmsFromUser: (userId: string): ApiRequestInfo => {
+      const token = keycloak.token;
       return {
         uri: new URL(`${postApiUrl}/user/${userId}`).toString(),
         options: {
           method: "GET",
-          headers: {}, // TODO authentication headers
+          headers: [["Authorization", `Bearer ${token}`]],
         },
       };
     },
@@ -59,6 +75,7 @@ export const postApi = {
       userId: string,
       searchTerm: string
     ): ApiRequestInfo => {
+      const token = keycloak.token;
       return {
         uri: new URL(
           `${postApiUrl}/user/${userId}?` +
@@ -66,16 +83,17 @@ export const postApi = {
         ).toString(),
         options: {
           method: "GET",
-          headers: {}, // TODO authentication headers
+          headers: [["Authorization", `Bearer ${token}`]],
         },
       };
     },
     postsFromGroup: (groupId: number): ApiRequestInfo => {
+      const token = keycloak.token;
       return {
         uri: new URL(`${postApiUrl}/group/${groupId}`).toString(),
         options: {
           method: "GET",
-          headers: {}, // TODO authentication headers
+          headers: [["Authorization", `Bearer ${token}`]],
         },
       };
     },
@@ -83,6 +101,7 @@ export const postApi = {
       groupId: number,
       searchTerm: string
     ): ApiRequestInfo => {
+      const token = keycloak.token;
       return {
         uri: new URL(
           `${postApiUrl}/group/${groupId}?` +
@@ -90,16 +109,17 @@ export const postApi = {
         ).toString(),
         options: {
           method: "GET",
-          headers: {}, // TODO authentication headers
+          headers: [["Authorization", `Bearer ${token}`]],
         },
       };
     },
     postsFromTopic: (topicId: number): ApiRequestInfo => {
+      const token = keycloak.token;
       return {
         uri: new URL(`${postApiUrl}/topic/${topicId}`).toString(),
         options: {
           method: "GET",
-          headers: {}, // TODO authentication headers
+          headers: [["Authorization", `Bearer ${token}`]],
         },
       };
     },
@@ -107,6 +127,7 @@ export const postApi = {
       topicId: number,
       searchTerm: string
     ): ApiRequestInfo => {
+      const token = keycloak.token;
       return {
         uri: new URL(
           `${postApiUrl}/topic/${topicId}?` +
@@ -114,18 +135,19 @@ export const postApi = {
         ).toString(),
         options: {
           method: "GET",
-          headers: {}, // TODO authentication headers
+          headers: [["Authorization", `Bearer ${token}`]],
         },
       };
     },
   },
   post: {
     newPost: (postDetails: TPostPost): ApiRequestInfo => {
+      const token = keycloak.token;
       return {
         uri: new URL(postApiUrl).toString(),
         options: {
           method: "POST",
-          headers: {}, // TODO authentication headers
+          headers: [["Authorization", `Bearer ${token}`]],
           body: JSON.stringify(postDetails),
         },
       };
@@ -133,11 +155,12 @@ export const postApi = {
   },
   put: {
     updatePost: (postId: number, postDetails: TPostPut): ApiRequestInfo => {
+      const token = keycloak.token;
       return {
         uri: new URL(`${postApiUrl}/${postId}`).toString(),
         options: {
           method: "PUT",
-          headers: {}, // TODO authentication headers
+          headers: [["Authorization", `Bearer ${token}`]],
           body: JSON.stringify(postDetails),
         },
       };
