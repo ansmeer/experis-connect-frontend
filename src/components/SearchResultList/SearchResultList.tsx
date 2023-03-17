f97871f8c06126c17fa47249ccd8319c4586abcb
Luisa <luisa.mohle@no.experis.com>
2023-03-17 16:20:20 +0100

    feat: search result list

--- src/components/SearchResultList/SearchResultList.tsx
+++ src/components/SearchResultList/SearchResultList.tsx
@@ -1,55 +1,39 @@
-import React, { useEffect } from "react";
-import Post from "../Post/Post";
 import { useSearchParams } from "react-router-dom";
-import {useState} from "react"
 import { postApi } from "../../apis/postApi";
 import { useQuery } from "react-query";
-import { useNavigate } from "react-router-dom";
 import { TPostWithReplies } from "../../types/post";
-
+import Loading from "../Loading/Loading";
+import PostList from "../PostList/PostList";
 
 function SearchResultList() {
-
-
   const [searchParams, setSearchParams] = useSearchParams();
-  const[list ,setList]= useState();
-  
-  const navigate = useNavigate();
-
-  
- 
-  let isLoaded: Boolean=false;
-  let urlParam: string | null = searchParams.get("search")
-  
-
- let variable = fetch(`http://localhost:8080/api/v1/post`)
-  .then(res => res.json())
-  .then(res => setList(res))
+  const urlParam = searchParams.get("search");
 
   const { data, isLoading, isError } = useQuery<TPostWithReplies[]>({
-    queryKey: ["search", urlParam ],
+    queryKey: ["search", urlParam],
     queryFn: async () => {
-      if(!urlParam)return;
+      if (!urlParam) return;
       const postRequest = postApi.get.searchPosts(urlParam);
-      console.log(postRequest.uri)
       const response = await fetch(postRequest.uri, postRequest.options);
-      if (response.status === 404) {
-        navigate("/404");
-      }
       return await response.json();
     },
   });
 
-  const searchResults= data?.map(result => <div>{result.id}</div>)
+  const hasResults = Boolean(data?.length);
 
+  if (isLoading) {
+    return <Loading />;
+  }
 
-  
-  
   return (
     <>
-    
-      <div>{urlParam}</div>
-      <div>{searchResults}</div>
+      <div>Search results for: "{urlParam}"</div>
+      {isError && <div>Could not fetch search results</div>}
+      {hasResults && data ? (
+        <PostList data={data} />
+      ) : (
+        <div>No results found</div>
+      )}
     </>
   );
 }
