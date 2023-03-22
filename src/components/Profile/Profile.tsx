0894f6b18b1472892d4a16c6e9787c7e4d5f7369
william-vil <vilhelmsennn@gmail.com>
2023-03-22 11:18:48 +0100

    Feat: add styling profile page and settings page

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,14 +1,17 @@
 import styles from "./profile.module.css";
+import { logout } from "../../redux/slices/userSlice";
 import { TUser } from "../../types/user";
 import { useNavigate, useParams } from "react-router-dom";
 import { userApi } from "../../apis/userApi";
 import { useQuery } from "react-query";
 import Loading from "../Loading/Loading";
-import { useSelector } from "react-redux";
+import keycloak from "../../utils/keycloak";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import ErrorFetch from "../ErrorFetch/ErrorFetch";
 
 function Profile() {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((state: RootState) => state.user.details);
   const { id } = useParams(); // TODO id not updated when going from other profile to own profile
@@ -28,6 +31,11 @@ function Profile() {
     navigate("/profile/settings");
   };
 
+  const onLogoutClick = () => {
+    dispatch(logout());
+    keycloak.logout();
+  };
+
   if (isLoading) {
     return <Loading />;
   }
@@ -38,19 +46,54 @@ function Profile() {
 
   return (
     <div>
-      <h1>Profile</h1>
-      {data?.picture && (
-        <div>
-          <img src={data.picture} className={styles.avatar} alt={data.name} />
-        </div>
-      )}
-      {showEditButton && (
-        <button onClick={handleEditClick}>Edit profile settings</button>
-      )}
-      {data?.name && <div>Name: {data.name}</div>}
-      {data?.status && <div>Status: {data.status}</div>}
-      {data?.funFact && <div>Fun fact: {data.funFact}</div>}
-      {data?.bio && <div>Bio: {data.bio}</div>}
+      <h1>{data && data.name}</h1>
+      <section>
+        <header>
+          <div className={styles.profileBackground}>
+            <div className={styles.profilePicture2}>
+              <div className={styles.editButton}>
+                {showEditButton && (
+                  <button onClick={handleEditClick}>Edit</button>
+                )}
+              </div>
+            </div>
+            <div className={styles.profilePicture}>
+              {data?.picture && (
+                <img
+                  src={data.picture}
+                  className={styles.avatar}
+                  alt={data.name}
+                />
+              )}
+            </div>
+            <div className={styles.profileBackground2}>
+              {data?.name && (
+                <div className={styles.profileName}>{data.name}</div>
+              )}
+              {data?.status && (
+                <div className={styles.profileStatus}>
+                  <p className={styles.p}>{data.status}</p>
+                </div>
+              )}
+              {data?.bio && (
+                <div className={styles.profileBio}>
+                  Bio
+                  <p className={styles.p}>{data.bio}</p>
+                </div>
+              )}
+              {data?.funFact && (
+                <div className={styles.profileFunfact}>
+                  Fun fact
+                  <p className={styles.p}>{data.funFact}</p>
+                </div>
+              )}
+            </div>
+            <div className={styles.logoutButton}>
+              <button onClick={onLogoutClick}>Logout</button>
+            </div>
+          </div>
+        </header>
+      </section>
     </div>
   );
 }
